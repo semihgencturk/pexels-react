a35dca463b8da9c9ec97e3497517442c7509dee6
Semih Gencturk <semihgencturk@gmail.com>
2021-08-13 00:01:09 +0300

    Changed eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-    printWidth: 100,
+    trailingComma: "es5",
     tabWidth: 2,
     singleQuote: true,
-    semi: true,
-    trailingComma: 'all',
-    arrowParens: "always",
-    overrides: [
-        {
-            files: '*.{js,jsx,tsx,ts,scss,json,html}',
-            options: {
-                tabWidth: 4,
-            },
-        },
-    ],
+    jsxSingleQuote: true,
+    endOfLine: "auto"
 };
