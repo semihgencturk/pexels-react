3a17ad0b098a846cb65b52461e1b871be2a66dde
Semih Gencturk <semihgencturk@gmail.com>
2021-09-03 01:29:53 +0300

    Changed Api Key

--- src/api/getCuratedImages.tsx
+++ src/api/getCuratedImages.tsx
@@ -1,6 +1,6 @@
 function getCuratedImages(): Promise<any[]> {
   const apiKey: string =
-    '563492ad6f91700001000001a780e996c46543c4aaa1b447d96c7bd5';
+    '563492ad6f91700001000001280cef2af0e144eabfab3185be7121ca';
   return fetch(`https://api.pexels.com/v1/curated?per_page=18`, {
     method: 'GET',
     headers: new Headers({
